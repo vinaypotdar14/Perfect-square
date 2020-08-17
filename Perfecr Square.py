63fbbbf4268ac152fb870dc1a55cef6ea1a19998
Vinay Potdar <69380851+vinaypotdar14@users.noreply.github.com>
2020-08-18 00:30:57 +0530

    Version Updated

--- Perfecr Square.py
+++ Perfecr Square.py
@@ -1,24 +1,25 @@
 from math import*
 number1=input("enter your square number1")
 sqrt = (float(number1))**0.5
-rem= sqrt % 1
-#square= round(sqrt)**2
-if rem==0:
+rem1 = sqrt % 1
+if rem1 == 0:
     perfect_sqr=[number1]
 else: 0
+#square= round(sqrt)**2
+
 number2=input("enter your square number2")
 sqrt = (float(number2))**0.5
-rem= sqrt % 1
+rem2= sqrt % 1
 #square= round(sqrt)**2
-if rem==0:
+if rem2==0:
     perfect_sqr.append(number2)
 else:0
 
 number3=input("enter your square number3")
-sqrt = (float(number2))**0.5
-rem= sqrt % 1
+sqrt = (float(number3))**0.5
+rem3= sqrt % 1
 #square= round(sqrt)**2
-if rem==0:
+if rem3==0:
     perfect_sqr.append(number3)
     print(perfect_sqr)
 else:
